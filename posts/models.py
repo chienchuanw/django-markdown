e001d46d816642ef1c03d31b76351090040d62a5
chienchuanw <chienchuanwww@gmail.com>
2024-07-05 15:47:47 +0800

    Created class 'Post' and registered in admin.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name="標題")
+    content = models.TextField(verbose_name="內文")
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return self.title
