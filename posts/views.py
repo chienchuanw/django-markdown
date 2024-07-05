860f5ec913dcb52645e28640680bf641f36066ea
chienchuanw <chienchuanwww@gmail.com>
2024-07-05 17:13:13 +0800

    Configured 'post-detail' in views.py and urls.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def display_post(request):
+    post = Post.objects.first()
+    return render(request, "posts/detail.html", context={"post": post})
