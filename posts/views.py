34d6bdbf04c6f70cce359528274c6d1fcdeef244
chienchuanw <chienchuanwww@gmail.com>
2024-07-05 17:31:26 +0800

    Replaced safe filter with a custom 'markdown' filter

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-import markdown
 
 
 def display_post(request):
-    md = markdown.Markdown(extensions=["fenced_code"])
     post = Post.objects.first()
-    post.content = md.convert(post.content)
     return render(request, "posts/detail.html", context={"post": post})
