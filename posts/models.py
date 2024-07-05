8aa61dbb3fc55434aa5bfb404488097bcfe06ef0
chienchuanw <chienchuanwww@gmail.com>
2024-07-05 17:39:35 +0800

    Created a SlugField 'slug' in class Post

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name="標題")
     content = models.TextField(verbose_name="內文")
+    slug = models.SlugField(blank=True, unique=True, verbose_name="網址別名")
 
     class Meta:
         verbose_name = "Post"
